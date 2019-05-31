0511333bfd37cee8bb6499de6992999fa208bcf4
aybek can kaya <aybekcankaya@icloud.com>
2019-05-31 19:30:23 +0300

    init

---
+++ InstagramLikeGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramLikeGesture
+//
+//  Created by aybek can kaya on 31.05.2019.
+//  Copyright © 2019 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
